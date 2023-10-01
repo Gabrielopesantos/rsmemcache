551df8a84d3b2b158d9fc85da01884a3e400e471
Gabriel Santos <gabrielopesantos97@gmail.com>
2023-10-01 12:04:38 +0100

    Conn and client ping

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use std::io;
+use std::fmt::format;
+use std::io::{self, BufRead, Write};
 use std::net::{AddrParseError, SocketAddr, TcpStream};
 use std::str::FromStr;
 
@@ -7,8 +8,8 @@ use std::str::FromStr;
 pub struct Client {
     // Server address
     server_addr: SocketAddr,
-    // Tcp connection
-    conn: TcpStream,
+    // Server connection
+    conn: Conn,
     // Socket read/write timeout.
     timeout: u32,
 }
@@ -37,12 +38,51 @@ impl Client {
 
         Ok(Self {
             server_addr: socket_addr,
-            conn: tcp_stream,
+            conn: Conn::new(tcp_stream),
             timeout: 500,
         })
     }
+
+    pub fn ping(&mut self) -> Result<(), &'static str> {
+        match self.conn.write_read_line(b"version\r\n") {
+            Ok(_) => Ok(()),
+            Err(_) => Err("Failed to ping server"),
+        }
+    }
 }
 
+#[derive(Debug)]
+struct Conn {
+    stream: TcpStream,
+    reader: io::BufReader<TcpStream>,
+    writer: io::BufWriter<TcpStream>,
+}
+
+impl Conn {
+    fn new(stream: TcpStream) -> Self {
+        // FIXME: try_clone
+        Self {
+            stream: stream.try_clone().expect("Clone failed!"), // NOTE: Needed?
+            reader: io::BufReader::new(stream.try_clone().expect("Clone failed!")),
+            writer: io::BufWriter::new(stream.try_clone().expect("Clone failed!")),
+        }
+    }
+
+    fn write_read_line(&mut self, write_buf: &[u8]) -> Result<Vec<u8>, &'static str> {
+        if let Err(_) = self.writer.write_all(write_buf) {
+            return Err("Could not write buffer to stream");
+        }
+
+        let mut read_buf: Vec<u8> = Vec::new();
+        match self.reader.read_until(b'\n', &mut read_buf) {
+            Ok(bytes_read) => {
+                print!("Successfully read {} bytes", bytes_read);
+                Ok(read_buf)
+            }
+            Err(_) => return Err("Could not read from stream"),
+        }
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::Client;
@@ -56,4 +96,3 @@ mod tests {
         };
     }
 }
-
