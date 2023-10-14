c935e56d2004ed069a1906af49893ba090878cdf
Gabriel Santos <gabrielopesantos97@gmail.com>
2023-10-14 12:55:21 +0100

    add item command - wip

---
+++ src/item.rs
@@ -0,0 +1,20 @@
+#[derive(Debug)]
+pub struct Item {
+    pub key: String,
+    pub  value: Vec<u8>,
+    pub flags: u32,
+    pub expiration: i32,
+    pub cas_id: u64,
+}
+
+impl Item {
+    pub(crate) fn new(key: String, value: Vec<u8>, flags: u32, expiration: i32) -> Self {
+        Self {
+            key,
+            value,
+            flags,
+            expiration,
+            cas_id: 0,  //  NOTE: Add
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,24 @@
+mod item;
+
+use crate::item::Item;
 use std::io::{self, BufRead, Write};
 use std::net::{AddrParseError, SocketAddr, TcpStream};
 use std::str::FromStr;
+
+const DEFAULT_NET_TIMEOUT: u32 = 500;
+const DEFAULT_MAX_IDLE_CONNS: u8 = 2;
 
 #[allow(dead_code)]
 #[derive(Debug)]
 pub struct Client {
     // Server address
     server_addr: SocketAddr,
-    // Server connection
-    conn: Conn,
+    // Server connections
+    conns: Vec<Conn>,
     // Socket read/write timeout.
     timeout: u32,
+    // Max idle connections
+    max_idle_cons: u8,
 }
 
 #[derive(Debug)]
@@ -32,21 +40,90 @@ impl From<io::Error> for ClientConnError {
 }
 
 impl Client {
-    pub fn new(server_addr: String) -> Result<Self, ClientConnError> {
+    pub fn new(
+        server_addr: String,
+        timeout: u32,
+        max_idle_conns: u8,
+    ) -> Result<Self, ClientConnError> {
         let socket_addr = SocketAddr::from_str(&server_addr)?;
         let tcp_stream = TcpStream::connect(socket_addr)?;
 
+        let mut server_conns: Vec<Conn> = Vec::new();
+        // NOTE: Lazily create connections or start with one?
+        server_conns.push(Conn::new(tcp_stream));
+
         Ok(Self {
             server_addr: socket_addr,
-            conn: Conn::new(tcp_stream),
-            timeout: 500,
+            conns: server_conns,
+            timeout: Client::net_timout(timeout),
+            max_idle_cons: Client::max_idle_conns(max_idle_conns),
         })
     }
 
+    // TODO: Error
     pub fn ping(&mut self) -> Result<(), &'static str> {
-        match self.conn.write_read_line(b"version\r\n") {
+        // TODO: Select server
+        match self.conns[0].write_read_line(b"version\r\n") {
             Ok(_) => Ok(()),
             Err(_) => Err("Failed to ping server"),
+        }
+    }
+
+    // TODO: Error
+    // NOTE: Item reference?
+    pub fn add(&mut self, item: Item) -> Result<(), &'static str> {
+        Client::populate_one(&mut self.conns[0], "add", item)
+    }
+
+    // TODO: Error
+    // TODO: returns?
+    // NOTE: Populate one what?
+    fn populate_one(conn: &mut Conn, verb: &str, item: Item) -> Result<(), &'static str> {
+        if !legal_key(&item.key) {
+            return Err("Invalid item key");
+        }
+        if let Err(_) = conn.writer.write_fmt(format_args!(
+            "{} {} {} {} {} {}\r\n",
+            verb,
+            item.key,
+            item.flags,
+            item.expiration,
+            item.value.len(),
+            item.cas_id
+        )) {
+            return Err("Could write set item command");
+        }
+        if let Err(_) = conn.writer.write_all(&item.value) {
+            return Err("Could write item");
+        }
+        if let Err(_) = conn.writer.flush() {
+            return Err("Could not send item to server");
+        }
+        let mut read_buf: Vec<u8> = Vec::new();
+        if let Err(_) = conn.reader.read_until(b'\n', &mut read_buf) {
+            return Err("Could not read server message");
+        }
+        if let Ok(line) = String::from_utf8(read_buf.clone()) {
+            match line.trim() {
+                "STORED" => Ok(()),
+                _ => Err("TODO"),
+            }
+        } else {
+            Err("Could not parse the returned message")
+        }
+    }
+
+    fn net_timout(input_value: u32) -> u32 {
+        match input_value {
+            0 => DEFAULT_NET_TIMEOUT,
+            _ => input_value,
+        }
+    }
+
+    fn max_idle_conns(input_value: u8) -> u8 {
+        match input_value {
+            0 => DEFAULT_MAX_IDLE_CONNS,
+            _ => input_value,
         }
     }
 }
@@ -86,14 +163,24 @@ impl Conn {
         }
     }
 }
+
+fn legal_key(key: &String) -> bool {
+    if key.len() > 250 {
+        false;
+    }
+    true
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::item::Item;
+
     use super::Client;
     const LOCALHOST_TCP_ADDR: &str = "127.0.0.1:11211";
 
     #[test]
     fn invalid_server_addr_returns_err() {
-        let result = Client::new(String::from("alksdjasld"));
+        let result = Client::new(String::from("alksdjasld"), 0, 0);
         match result {
             Ok(_) => panic!("Expected creation of new client to fail"),
             Err(_) => (),
@@ -102,13 +189,18 @@ mod tests {
 
     #[test]
     fn test_local_host() {
-        let mut client = match Client::new(LOCALHOST_TCP_ADDR.to_string()) {
+        let mut client = match Client::new(String::from(LOCALHOST_TCP_ADDR), 0, 0) {
             Ok(client) => client,
             Err(error) => panic!("Could not connect to local server: {:?}", error),
         };
 
         if let Err(_) = client.ping() {
             panic!("Expected ping to succeed")
+        }
+
+        let item = Item::new(String::from("color"), Vec::from("red"), 0, 3600);
+        if let Err(_) = client.add(item) {
+            panic!("Expected item to be successfully persisted")
         }
     }
 }
